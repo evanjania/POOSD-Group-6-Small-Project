a8a5167e675009c426b5b636851424b623898a73
Logan <loganelkins0101@gmail.com>
2026-02-05 11:24:58 -0500

    Add Signup.php to handle adding users

---
+++ LAMPAPI/Signup.php
@@ -0,0 +1,61 @@
+<?php
+    // Creates body of json object with empty data and error message specifed by errorMessage
+    function sendError($errorMessage)
+    {
+        $obj = '{"id":0,
+                 "firstName":"",
+                 "lastName":"",
+                 "dateCreated":"",
+                 "vaultNum":0,
+                 "error":"' . $errorMessage . '"}';
+        sendResponse($obj);
+    }
+
+    // Adds header to json object specified by jsonObj and sends it to client
+    function sendResponse($jsonObj)
+    {
+        header('Content-type: application/json');
+		echo $jsonObj;
+    }
+    
+    // Get new user's first name, last name, login, and password
+    $inputs = json_decode(file_get_contents('php://input'), true);
+	
+    // Create MySQL db connection
+    $connection = new mysqli("localhost", "", "", "COP4331"); // Update with admin info
+    if($connection)
+    {
+        // Search for user in the database
+        $query = $connection->prepare("SELECT * FROM Users WHERE Login=? AND Password =?");
+        $query->bind_param("ss", $inputs["login"], $inputs["password"]);        
+        $query->execute();
+
+        $response = $query->get_result();
+
+        // If user exists in database send an error
+        if($userInfo = $response->fetch_assoc())
+        {
+            sendError("Username already exists in database");
+        }
+        else
+        {
+            // Insert user into database
+            $query = $connection->prepare("insert into Users (FirstName,LastName,Login,Password) VALUES 
+                                          (?,?,?,?);");
+            $query->bind_param("ssss", $inputs["firstName"], $inputs["lastName"], $inputs["login"], $inputs["password"]);        
+            $query->execute();
+
+            if(($response = $query->get_result()) == false)
+            {
+                sendError("Error adding user's info to database");
+            }
+        }
+
+        $query->close();
+        $connection->close();
+    }
+    else
+    {
+        sendError($connection->connect_error);
+    }
+?>
